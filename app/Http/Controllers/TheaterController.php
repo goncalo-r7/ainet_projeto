36278f8c3e146e6321b14658497f89e24fc2bc2e
Rodrigo Gomes <2221455@my.ipleiria.pt>
2024-05-25 20:29:49 +0100

    theatercontroller quase feito

--- app/Http/Controllers/TheaterController.php
+++ app/Http/Controllers/TheaterController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Theater;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
-//use App\Http\Requests\DisciplineFormRequest;
+use App\Http\Requests\TheaterFormRequest;
+//use App\Http\Requests\TheaterFormRequest;
 use Illuminate\Support\Facades\DB;
 
 
@@ -25,7 +26,7 @@ class TheaterController extends Controller
                 ->withQueryString();
             return view(
                 'theaters.index'
-            );
+            )->with($theaters);
         }
     
     
@@ -34,56 +35,55 @@ class TheaterController extends Controller
          */
         public function create(): View
         {
-            $discipline = new Discipline();
+            $theater = new Theater();
             // $courses no longer required, because it is available through View::share
             // Check AppServiceProvider
             //$courses = Course::all();
-            return view('disciplines.create')
-                ->with('discipline', $discipline);
-                //->with('courses', $courses);
+            return view('theaters.create')
+                ->with('theater', $theater);
         }
     
         /**
          * Store a newly created resource in storage.
          */
-        public function  store(DisciplineFormRequest $request): RedirectResponse
+        public function  store(TheaterFormRequest $request): RedirectResponse
         {
-            $NewDiscipline = Discipline::create($request->validated());
-            $url = route('disciplines.show', ['discipline' => $NewDiscipline]);
-            $htmlMessage = "Discipline <a href='$url'><u>{$NewDiscipline->name}</u></a> ({$NewDiscipline->abbreviation}) has been created successfully!";
-            return redirect()->route('disciplines.index')
-            ->with('alert-type', 'success')
-            ->with('alert-msg', $htmlMessage);
+             $Newtheater = theater::create($request->validated());
+             $url = route('theaters.show', ['theater' => $Newtheater]);
+             $htmlMessage = "Theater <a href='$url'><u>{$Newtheater->name}</u></a> ({$Newtheater->abbreviation}) has been created successfully!";
+             return redirect()->route('theaters.index')
+             ->with('alert-type', 'success')
+             ->with('alert-msg', $htmlMessage);
         }
     
     
         /**
          * Display the specified resource.
          */
-        public function show(Discipline $discipline): View
+        public function show(Theater $theaters): View
         {
-            return view('disciplines.show')
-                ->with('discipline', $discipline);
+            return view('theaters.show')
+                ->with('theater', $theaters);
         }
     
         /**
          * Show the form for editing the specified resource.
          */
-        public function edit(Discipline $discipline): View
+        public function edit(Theater $theaters): View
         {
-            return view('disciplines.edit')
-                ->with('discipline', $discipline);
+            return view('theaters.edit')
+                ->with('theater', $theaters);
         }
     
         /**
          * Update the specified resource in storage.
          */
-        public function update(DisciplineFormRequest $request, Discipline $discipline): RedirectResponse
+        public function update(TheaterFormRequest $request, Theater $theater): RedirectResponse
         {
-            $discipline->update($request->validated());
-            $url = route('disciplines.show', ['discipline' => $discipline]);
-            $htmlMessage = "Discipline <a href='$url'><u>{$discipline->name}</u></a> ({$discipline->abbreviation}) has been updated successfully!";
-            return redirect()->route('disciplines.index')
+            $theater->update($request->validated());
+            $url = route('theaters.show', ['theater' => $theater]);
+            $htmlMessage = "theater <a href='$url'><u>{$theater->name}</u></a> has been updated successfully!";
+            return redirect()->route('theaters.index')
                 ->with('alert-type', 'success')
                 ->with('alert-msg', $htmlMessage);
         }
@@ -92,46 +92,45 @@ class TheaterController extends Controller
         /**
          * Remove the specified resource from storage.
          */
-        public function destroy(Discipline $discipline): RedirectResponse
+        public function destroy(Theater $theater): RedirectResponse
         {
             try {
-                $url = route('disciplines.show', ['discipline' => $discipline]);
-                $totalStudents = DB::scalar(
-                    'select count(*) from students_disciplines where discipline_id = ?',
-                    [$discipline->id]
+                $url = route('theaters.show', ['theater' => $theater]);
+                $totalScreenings = DB::scalar(
+                    'select count(*) from screenings where theater_id = ?',
+                    [$theater->id]
                 );
-                $totalTeachers = DB::scalar(
-                    'select count(*) from teachers_disciplines where discipline_id = ?',
-                    [$discipline->id]
+                $totalSeats = DB::scalar(
+                    'select count(*) from seats where theater_id = ?',
+                    [$theater->id]
                 );
-                if ($totalStudents == 0 && $totalTeachers == 0) {
-                    $discipline->delete();
+                if ($totalScreenings == 0 && $totalSeats == 0) {
+                    $theater->delete();
                     $alertType = 'success';
-                    $alertMsg = "Discipline {$discipline->name} ({$discipline->abbreviation}) has been deleted successfully!";
+                    $alertMsg = "theater {$theater->name} has been deleted successfully!";
                 } else {
                     $alertType = 'warning';
-                    $studentsStr = match (true) {
-                        $totalStudents <= 0 => "",
-                        $totalStudents == 1 => "there is 1 student enrolled in it",
-                        $totalStudents > 1 => "there are $totalStudents students enrolled in it",
+                    $screeningsStr = match (true) {
+                        $totalScreenings == 1 => "there is 1 screening in this theater",
+                        $totalScreenings > 1 => "there are $totalScreenings screening in this theater",
                     };
-                    $teachersStr = match (true) {
-                        $totalTeachers <= 0 => "",
-                        $totalTeachers == 1 => "it already has 1 teacher",
-                        $totalTeachers > 1 => "it already has $totalTeachers teachers",
+                    $seatsStr = match (true) {
+                        $totalSeats <= 0 => "",
+                        $totalSeats == 1 => "it already has 1 teacher",
+                        $totalSeats > 1 => "it already has $totalSeats teachers",
                     };
-                    $justification = $studentsStr && $teachersStr
-                        ? "$teachersStr and $studentsStr"
-                        : "$teachersStr$studentsStr";
-                    $alertMsg = "Discipline <a href='$url'><u>{$discipline->name}</u></a> ({$discipline->abbreviation}) cannot be deleted because $justification.";
+                    $justification = $screeningsStr && $seatsStr
+                        ? "$seatsStr and $screeningsStr"
+                        : "$seatsStr$screeningsStr";
+                    $alertMsg = "theater <a href='$url'><u>{$theater->name}</u></a> cannot be deleted because $justification.";
                 }
             } catch (\Exception $error) {
                 $alertType = 'danger';
-                $alertMsg = "It was not possible to delete the discipline
-                                <a href='$url'><u>{$discipline->name}</u></a> ({$discipline->abbreviation})
+                $alertMsg = "It was not possible to delete the theater
+                                <a href='$url'><u>{$theater->name}</u></a>
                                 because there was an error with the operation!";
             }
-            return redirect()->route('disciplines.index')
+            return redirect()->route('theaters.index')
                 ->with('alert-type', $alertType)
                 ->with('alert-msg', $alertMsg);
         }
