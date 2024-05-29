5bc2a9d88b88d51b48ffdb1ff38b06dcd8e9422e
Gonçalo Rodrigues <2221440@my.ipleiria.pt>
2024-05-29 20:17:04 +0100

    tabela em seats

--- resources/views/seats/index.blade.php
+++ resources/views/seats/index.blade.php
@@ -13,7 +13,7 @@
             @endphp
             @csrf
             <!-- Ao submeter, criar tickets e adicionar ao carrinho-->
-            {{-- action="{{ route('cart.add', ['discipline' => $discipline]) }}" --}}
+            
             
             {{-- <form id="seat-form" method="POST" action="/submit-seats">
                 <x-button type="submit" class="mt-4 px-4 py-2" text='Comprar' type='primary'></x-button>
@@ -42,40 +42,39 @@
                 @endforeach
                 </div>
             </form> --}}
-
-            <form id="seat-form" method="POST" action="/submit-seats">
-                <x-button type="submit" class="mt-4 px-4 py-2" text='Comprar' type='primary'></x-button>
-                    <table class="table-auto border-collapse">
-                        <div class="flex flex-col gap-4 mb-4 overflow-auto">
-                        @foreach ($seatsByRow as $row => $seats)
-                            <div class="flex items-center gap-4">
-                                <tr class="flex items-center space-x-2">
-                                @foreach ($seats as $seat)
-                                    @php
-                                        $isTaken = $screeningSession->tickets->where('seat_id', $seat->id)->isNotEmpty();
-                                    @endphp
-                                    
-                                    
-                                        <td class="px-2 py-2 text-left hidden sm:table-cell text-gray-900 dark:text-gray-100">
-                                        <input
-                                            type="checkbox"
-                                            name="selectedSeats[]"
-                                            value="{{ $seat->id }}"
-                                            class="seat-checkbox 
-                                            {{ $isTaken ? 'cursor-not-allowed' : ''}}"
-                                            {{ $isTaken ? ' checked ' : '' }}
-                                            {{ $isTaken ? ' disabled ' : ''}}
-                                        >
-                                        <span>{{ $seat->row . $seat->seat_number }}</span>
-                                        </td>
-                                    
-                                @endforeach
-                            </tr>
-                            </div>
-                        @endforeach
+            
+            {{-- when the form is submitted, the IDs of the selected seats will be sent as an array with the name "selectedSeats" --}}
+            {{-- action="{{ route('cart.add', ['discipline' => $discipline]) }}" --}}
+            <form method="POST" action="">
+                <x-button type="submit" class="mt-4 px-4 py-2" text='Comprar' type='primary'/>
+                <table class="table-auto border-collapse">
+                    <div class="flex flex-col gap-4 mb-4 overflow-auto">
+                    @foreach ($seatsByRow as $row => $seats)
+                        <div class="flex items-center gap-4">
+                            <tr class="flex items-center space-x-2">
+                            @foreach ($seats as $seat)
+                                @php
+                                    $isTaken = $screeningSession->tickets->where('seat_id', $seat->id)->isNotEmpty();
+                                @endphp
+                                
+                                <td class="px-2 py-2 text-left hidden sm:table-cell text-gray-900 dark:text-gray-100">
+                                <input
+                                    type="checkbox"
+                                    name="selectedSeats[]"
+                                    value="{{ $seat->id }}"
+                                    class="{{ $isTaken ? 'cursor-not-allowed' : ''}}"
+                                    {{ $isTaken ? ' checked ' : '' }}
+                                    {{ $isTaken ? ' disabled ' : ''}}
+                                >
+                                <span>{{ $seat->row . $seat->seat_number }}</span>
+                                </td>                           
+                            @endforeach
+                        </tr>
                         </div>
-                    </table>
-                </form>
+                    @endforeach
+                    </div>
+                </table>
+            </form>
             {{-- <x-button
                                 class="flex h-10 w-14"
                                 text="{{ $seat->row . $seat->seat_number }}"    
