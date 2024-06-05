b1c836f3a9cbb5e6ce302fa6e744971314758845
luiss <2221441@my.ipleiria.pt>
2024-06-05 08:38:05 +0100

    s

--- resources/views/components/screenings/table.blade.php
+++ resources/views/components/screenings/table.blade.php
@@ -3,7 +3,7 @@
         <thead>
             <tr class="border-b-2 border-b-gray-400 dark:border-b-gray-500 bg-gray-100 dark:bg-gray-800">
                 <th class="px-2 py-2 text-center">Theatre</th>
-                <th class="px-2 py-2 text-center hidden md:table-cell"></th>
+                <th class="px-2 py-2 text-center hidden md:table-cell">date</th>
                 <th class="px-2 py-2 text-center hidden lg:table-cell">Start time</th>
                 <th class="px-2 py-2 text-center"></th>
             </tr>
