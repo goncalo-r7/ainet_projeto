9e109a9c935231504541070a91a5363eabf7a327
Gonçalo Rodrigues <2221440@my.ipleiria.pt>
2024-06-15 01:18:42 +0100

    cart.show and confirm

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -3,6 +3,7 @@
 @section('header-title', 'Shopping Cart')
 
 @section('main')
+@php use App\Models\Customer; @endphp
     <div class="flex justify-center">
         <div class="my-4 p-6 bg-white dark:bg-gray-900 overflow-hidden
                     shadow-sm sm:rounded-lg text-gray-900 dark:text-gray-50">
@@ -21,12 +22,52 @@
             <div class="mt-12">
                 <div class="flex justify-between space-x-12 items-end">
                     <div>
-                        <h3 class="mb-4 text-xl">Shopping Cart Confirmation </h3>
+                        @php
+                        $user = Auth::user();
+                        $customerId = Customer::where('id', $user->id)->first();
+                        $conf = DB::table('configuration')->first();
+                        $price = $conf->ticket_price * count($cart);
+                        $isCustomer = false;
+                        if ($customerId) {
+                            $isCustomer = true;
+                            $discount = $conf->registered_customer_ticket_discount * count($cart);
+                            $customerName = $user->name;
+                            $customerEmail = $user->email;
+                            $customerNif = $customerId->nif;
+                            $paymentType = $customerId->payment_type;
+                            $paymentRef = $customerId->payment_ref;
+                        }
+                        @endphp
+                        <h3 class="mb-4 text-xl"><b>Shopping Cart Confirmation</b></h3>
                         <form action="{{ route('cart.confirm') }}" method="post">
                             @csrf
-                                <x-field.input name="student_number" label="Student Number" width="lg"
+                                <h4 class="mb-4 text-lg">Price:
+                                @php
+                                    if ($isCustomer) {
+                                        echo "<s>" . $price . "</s> " . ($price - $discount);
+                                    } else {
+                                        echo $price;
+                                    }
+                                @endphp €</h4>
+                                <x-field.input name="customer_name" label="Name" width="lg"
+                                                :readonly="$isCustomer"
+                                                value="{{ $isCustomer ? $customerName : old('customer_name') }}"/>
+                                <x-field.input name="customer_email" label="E-Mail" width="lg"
+                                                :readonly="$isCustomer"
+                                                value="{{ $isCustomer ? $customerEmail : old('customer_email') }}"/>
+                                <x-field.input name="customer_nif" label="NIF (optional)" width="sm"
+                                                :readonly="$isCustomer"
+                                                value="{{ $isCustomer ? $customerNif : old('customer_nif') }}"/><br>
+                                <h3 class="mb-4 text-xl">Payment Details</h3>
+                                <x-field.radio-group name="payment_type" label="Payment Type" width="lg"
+                                                    :options="['VISA' => 'Visa Card',
+                                                               'PAYPAL' => 'PayPal',
+                                                               'MBWAY' => 'MB Way']" 
+                                                    :value="$paymentType ?? old('payment_type')"/>
+                                                    {{-- if not null, value is $customerPaymentType --}}
+                                <x-field.input name="payment_ref" label="Payment Reference" width="md"
                                                 :readonly="false"
-                                                value="{{ old('student_number') }}"/>
+                                                value="{{ $paymentRef ?? old('payment_ref') }}"/>
                                 <x-button element="submit" type="dark" text="Confirm" class="mt-4"/>
                         </form>
                     </div>
--- resources/views/components/screenings/table.blade.php
+++ resources/views/components/screenings/table.blade.php
@@ -3,8 +3,8 @@
         <thead>
             <tr class="border-b-2 border-b-gray-400 dark:border-b-gray-500 bg-gray-100 dark:bg-gray-800">
                 <th class="px-2 py-2 text-center">Theatre</th>
-                <th class="px-2 py-2 text-center hidden md:table-cell">Date</th>
-                <th class="px-2 py-2 text-center hidden lg:table-cell">Start time</th>
+                <th class="px-2 py-2 text-center hidden lg:table-cell">Date</th>
+                <th class="px-2 py-2 text-center hidden md:table-cell">Start time</th>
                 <th class="px-2 py-2 text-center"></th>
                 @if ($showView)
                     <th></th>
@@ -22,8 +22,7 @@
                 <tr class="border-b border-b-gray-400 dark:border-b-gray-500">
                     <td class="px-2 py-2 text-center">{{ $screening->theater->name }}</td>
                     <td class="px-2 py-2 text-center hidden md:table-cell">{{ $screening->date }}</td>
-                    <td class="px-2 py-2 text-center hidden md:table-cell">{{ substr($screening->start_time, 0, 5) }}
-                    </td>
+                    <td class="px-2 py-2 text-center hidden md:table-cell">{{ substr($screening->start_time, 0, 5) }}</td>
                     <td class="px-2 py-2 flex justify-center items-center">
                         <x-table.icon-seat class="px-0.5"
                             href="{{ route('seats.index', ['screening' => $screening->id]) }}" />
