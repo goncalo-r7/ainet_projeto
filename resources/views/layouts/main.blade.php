2fc2d41503d1eb6d3bfda1cfa2f00574c21fcd10
Rodrigo Gomes <2221455@my.ipleiria.pt>
2024-06-16 23:14:36 +0100

    botão "Tickets" adicionado no menu principal

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,13 +45,10 @@
                             <!-- Menu Item: Configuration -->
                         <x-menus.menu-item content="Configuration" href="{{ route('configurations.show') }}"
                         selected="{{ Route::currentRouteName() == 'configurations.show' }}" />
-
-
-
-
                         @auth
 
-
+                        <x-menus.menu-item content="Tickets" href="{{ route('tickets.index') }}"
+                        selected="{{ Route::currentRouteName() == 'tickets.index' }}" />
                             <!-- Menu Item: Others -->
                             <x-menus.submenu selectable="0" uniqueName="submenu_others" content="More">
 
