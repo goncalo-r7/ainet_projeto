f49bc819ebd7f0c681892eac22cd65486548549c
Gonçalo Rodrigues <2221440@my.ipleiria.pt>
2024-06-17 22:27:36 +0100

    ...

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CineMagic Cinemas</title>
     <style>
-      <?=file_get_contents(public_path('build/assets/app-Bj4pT88W.css')); ?>
+      <?=file_get_contents(public_path('build/assets/app-BPpGcTh_.css')); ?>
     </style>
 </head>
 
